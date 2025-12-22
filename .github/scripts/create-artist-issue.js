1cc4787c5147ba6c1647365aa21293b364465a88
romiem <rohit.mistry@gmail.com>
2025-12-22 15:18:16 +0000

    Properties now wrapped in a data object

--- .github/scripts/create-artist-issue.js
+++ .github/scripts/create-artist-issue.js
@@ -6,25 +6,25 @@ const octokit = new Octokit({
 
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
 const payload = JSON.parse(process.env.PAYLOAD);
-const confidenceScore = payload.confidenceScore;
+const data = payload.data;
 
 const issueBody = [
-  `{name}: ${payload.name}`,
-  `{comments}: ${payload.comments}`,
-  `{tags}: ${payload.tags}`,
-  `{spotify}: ${payload.spotify}`,
-  `{apple}: ${payload.apple}`,
-  `{amazon}: ${payload.amazon}`,
-  `{youtube}: ${payload.youtube}`,
-  `{instagram}: ${payload.instagram}`,
-  `{tiktok}: ${payload.tiktok}`,
-  `{urls}: ${payload.urls}`,
+  `{name}: ${data.name}`,
+  `{comments}: ${data.comments}`,
+  `{tags}: ${data.tags}`,
+  `{spotify}: ${data.spotify}`,
+  `{apple}: ${data.apple}`,
+  `{amazon}: ${data.amazon}`,
+  `{youtube}: ${data.youtube}`,
+  `{instagram}: ${data.instagram}`,
+  `{tiktok}: ${data.tiktok}`,
+  `{urls}: ${data.urls}`,
 ].join('\n');
 
 octokit.issues.create({
   owner,
   repo,
-  title: `Artist Submission: ${payload.name}`,
+  title: `Artist Submission: ${data.name}`,
   body: issueBody,
-  labels: [confidenceScore >= 3 ? 'artist-submission:high' : 'artist-submission:low'],
+  labels: [data.confidenceScore >= 3 ? 'artist-submission:high' : 'artist-submission:low'],
 });
